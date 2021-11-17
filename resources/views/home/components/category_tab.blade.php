bd23252a1ca2e681d4a69c3df8e9450a3867fb69
phantomXXI <haianhlol2001@gmail.com>
2021-11-17 09:53:01 +0700

    Lay du lieu san pham trong tabs

---
+++ resources/views/home/components/category_tab.blade.php
@@ -0,0 +1,40 @@
+<div class="category-tab">
+    <div class="col-sm-12">
+        <ul class="nav nav-tabs">
+            @foreach($categorys as $indexCategory => $categoryItem)
+            <li class="{{ $indexCategory == 0 ? 'active' : '' }}">
+                <a href="#category_tab_{{ $categoryItem->id }}" data-toggle="tab">
+                    {{ $categoryItem->name }}
+                </a>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+
+
+    <div class="tab-content">
+
+        @foreach($categorys as $indexCategoryProduct => $categoryItemProduct)
+            <div class="tab-pane fade {{ $indexCategoryProduct == 0 ? 'active in' : '' }}" id="category_tab_{{ $categoryItemProduct->id }}" >
+
+            @foreach($categoryItemProduct->products as $productItemTabs)
+                <div class="col-sm-3">
+                <div class="product-image-wrapper">
+                    <div class="single-products">
+                        <div class="productinfo text-center">
+                            <img src="/eshopper/images/home/gallery1.jpg" alt="" />
+                            <h2>$56</h2>
+                            <p>{{ $productItemTabs->name }}</p>
+                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
+        @endforeach
+
+    </div>
+</div>
